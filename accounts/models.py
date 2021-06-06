a30883b43bdd6066446cf768cf6a4db80cae2c44
MehediBijoy <mehedibubt34@gmail.com>
2021-06-06 18:38:07 +0600

    Initial commit

---
+++ accounts/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class MyAccountManager(BaseUserManager):
+    def create_user(self, first_name, last_name, username, email, password=None):
+        if not email:
+            raise ValueError('user must have an email address')
+
+        if not username:
+            raise ValueError('user must have an username')
+
+        user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=self.normalize_email(email)
+        )
+
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, first_name, last_name, username, email, password=None):
+        user = self.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            email=self.normalize_email(email),
+            password=password
+        )
+
+        user.is_admin = True
+        user.is_superadmin = True
+        user.is_staff = True
+        user.is_active = True
+        user.save(using=self.db)
+        return user
+
+
+class Accounts(AbstractBaseUser):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
+    phone_number = models.CharField(max_length=50)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_superadmin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(slef, add_label):
+        return True
+
+    class Meta:
+        verbose_name = 'account'
+        verbose_name_plural = 'accounts'
