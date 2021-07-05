c7000737025d1e88d2ef805b874bf5102312a386
Mehedi Hasan Bijoy <mehedibubt34@gmail.com>
2021-07-05 15:50:12 +0600

    order updated

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from accounts.models import Accounts
+from store.models import Products, Variation
 
-# Create your models here.
+
+class Payment(models.Model):
+    user = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    payment_id = models.CharField(max_length=100)
+    payment_method = models.CharField(max_length=100)
+    amount_paid = models.CharField(max_length=18)
+    status = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.payment_id
+
+
+class Order(models.Model):
+    STATUS = (
+        ('now', 'now'),
+        ('accepted', 'accepted'),
+        ('completed', 'completed'),
+        ('cancelled', 'cancelled')
+    )
+
+    user = models.ForeignKey(Accounts, on_delete=models.CASCADE, null=True)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, null=True)
+    order_number = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(max_length=50)
+    address_line1 = models.CharField(max_length=50)
+    address_line2 = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    order_note = models.TextField(max_length=500, blank=True)
+    order_total = models.FloatField()
+    tax = models.FloatField()
+    status = models.CharField(max_length=50, choices=STATUS, default='now')
+    ip = models.CharField(max_length=100, blank=True)
+    is_ordered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.first_name
+    
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    variation = models.ForeignKey(Variation, on_delete=models.CASCADE)
+    color = models.CharField(max_length=20)
+    size = models.CharField(max_length=20)
+    quantity = models.IntegerField()
+    price = models.FloatField()
+    ordered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.product_name
