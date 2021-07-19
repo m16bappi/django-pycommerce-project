354cdcf4ee91afc49806e3e840a2d3fa3f327e2b
Mehedi Hasan <mehedibubt34@gmail.com>
2021-07-20 01:17:31 +0600

    update

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('', placeOrder, name='place-order')
+    path('', placeOrder, name='place-order'),
+    path('<int:order_number>/', payment, name='payment'),
+    path('payment/<int:order_number>/', completePayment, name='complete-payment')
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,53 @@
-from django.shortcuts import redirect, render, HttpResponse
-from carts.models import Cart_Item
-from .models import Order
-from .forms import orderForm
 import datetime
+from django.urls import reverse
+from django.shortcuts import redirect, render, HttpResponseRedirect
+from django.contrib import messages
+
+from carts.models import Cart_Item
+from .models import Order, Payment, OrderProduct
+from .forms import orderForm
+
+
+def payment(request, order_number):
+    cart_items = Cart_Item.objects.filter(user=request.user)
+    order = Order.objects.get(
+        user=request.user, is_ordered=False, order_number=order_number)
+    context = {
+        'order': order,
+        'cart_items': cart_items,
+        'total': order.order_total - order.tax,
+        'tax': order.tax,
+        'grand_total': order.order_total,
+        'order_number': order.order_number
+    }
+    return render(request, 'payment.html', context)
+
+
+def completePayment(request, order_number):
+    order = Order.objects.get(
+        user=request.user, is_ordered=False, order_number=order_number)
+    print(order)
+    payment = Payment.objects.create(
+        user=request.user,
+        payment_id=order_number,
+        payment_method='PayPal',
+        amount_paid=order.order_total,
+        status='Verified'
+    )
+    payment.save()
+    order.payment = payment
+    order.is_active = True
+    order.save()
+
+    # move cart item to the order product table
+
+    # reduce the quantity of the products
+
+    # sent mail to the user
+
+    # clear cart item from cart
+
+    return redirect('dashboard')
 
 
 def placeOrder(request, total=0, quantity=0):
@@ -24,6 +69,7 @@ def placeOrder(request, total=0, quantity=0):
         form = orderForm(request.POST)
         if form.is_valid():
             data = Order()
+            data.user = request.user
             data.first_name = form.cleaned_data['first_name']
             data.last_name = form.cleaned_data['last_name']
             data.phone = form.cleaned_data['phone']
@@ -47,6 +93,10 @@ def placeOrder(request, total=0, quantity=0):
             order_number = current_date + str(data.id)
             data.order_number = order_number
             data.save()
+            messages.success(request, 'your order placed successfully')
+            return HttpResponseRedirect(reverse('payment', args=[order_number]))
+        else:
+            messages.error(request, 'please complete your forms')
             return redirect('checkout')
     else:
         return redirect('checkout')
