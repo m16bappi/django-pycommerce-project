bfd8f30cb79ce59f800ac387f342929587cc5cb9
Mehedi Hasan <mehedibubt34@gmail.com>
2022-06-11 13:48:34 +0600

    Update settings.py

--- pycom/settings.py
+++ pycom/settings.py
@@ -154,8 +154,8 @@ MESSAGE_TAGS = {
 # smtp configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mehedibijoy34@gmail.com'
-EMAIL_HOST_PASSWORD = '01730687555'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
